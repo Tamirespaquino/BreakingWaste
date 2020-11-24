89636115aad6cf24ee468fe21b8230f915791f0e
carolbazzo <anacarolb@gmail.com>
2020-11-24 20:56:01 -0300

    Adição do sequelize, models e configuração do config, sequelizesrc e adição de arquivos de model de acordo com o banco

---
+++ ExpressBreakingWaste/config/database.js
@@ -0,0 +1,11 @@
+const { DatabaseError } = require("sequelize/types");
+
+const config = {
+    username:'root',
+    password:'',
+    database:'node.js',
+    host:'localhost',
+    dialect:'mysql',
+}
+
+module.exports = config
