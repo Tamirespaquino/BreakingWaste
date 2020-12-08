d023368297aded1a464c116efa7c512b39487018
carolbazzo <anacarolb@gmail.com>
2020-12-08 13:24:35 -0300

    correção do crud, controller e rota

--- ExpressBreakingWaste/controllers/Perfilusuario_Controller.js
+++ ExpressBreakingWaste/controllers/Perfilusuario_Controller.js
@@ -4,75 +4,102 @@ const { createInflateRaw } = require('zlib');
 
 
 module.exports = {
-    entre: function(req, res, next) {
-        res.render('perfil_usuario');
+    entre: function (req, res, next) {
+        res.render('perfilusuario');
     },
 
-    visualizar: function(req, res, next) {
-        let empresa = empresas.findAll((empresa) => {
-            empresas.id == id;
-            res.render('perfil_usuario');
-        });
-
-        res.redirect('perfil_usuario');
+    visualizarempresa: function (req, res, next) {
+        const id = req.params.id;
+        const empresa = empresa.find(empresa => empresa.id == id);
+        res.render('perfilusuario/mostrar', { empresa });
     },
 
 
-    editar: function(req, res, next) {
-        let empresa = empresas.find((empresa) => {
-            empresas.id == id;
+    editar: function (req, res, next) {
+        const id = req.params.id;
+        const empresa = empresa.find(empresa => empresa.id == id);
+        res.render('perfilusuario/editar', { empresa });
+    },
+
+    alterar: function (req, res, next) {
+        const id = req.params.id;
+        const empresa = empresa.find(empresa => empresa.id == id);
+
+        email = req.body.email;
+        senha = password_hashed;
+        razao = req.body.razaoSocial;
+        cnpj = req.body.cnpj;
+        telefone = req.body.cnpj;
+        endereco = req.body.endereco;
+        numero = req.body.endereco;
+        bairro = req.body.bairro;
+        complemento = req.body.bairro;
+        cidade = req.body.cidade;
+        estado = req.body.estado;
+        cep = req.body.cep;
+        funcionamento = res.body.funcionamento;
+        tipousuario = req.body.tipousuario;
+        quantidadebombonas = req.body.quantidadebombonas;
+        tamanhobombonas = req.body.tamanhobombonas;
+        tiposresiduos = req.body.tiposresiduos;
+
+    },
+
+    deletar: function (req, res, next) {
+        const id = req.params.id;
+        const perfil = empresas.findIndex((empresa) => empresa.id == id);
+
+        if (perfil === -1) return res.redirect('/perfilususario');
+
+        empresas.splice(perfil, 1);
+
+        res.redirect('/perfilusuario');
+    },
+
+    criarnovo: function (req, res, next) {
+        res.render('perfilusuario/criar');
+    },
+
+    guardarnovo: function (req, res, next) {
+        let { email,
+            senha,
+            razao,
+            cnpj,
+            telefone,
+            endereco,
+            numero,
+            bairro,
+            complemento,
+            cidade,
+            estado,
+            cep,
+            funcionamento,
+            tipousuario,
+            quantidadebombinas,
+            tamanhobombinas,
+            tiposresiduos } = res.body;
+
+        empresas.push({
+            id: empresas.reverse()[0].id + 1,
+            email,
+            senha,
+            razao,
+            cnpj,
+            telefone,
+            endereco,
+            numero,
+            bairro,
+            complemento,
+            cidade,
+            estado,
+            cep,
+            funcionamento,
+            tipousuario,
+            quantidadebombinas,
+            tamanhobombinas,
+            tiposresiduos
         })
 
-        empresas.get('editar', function(req, res, next) {
-            const id = req.params.id;
-        
-            let empresa = empresas.find(empresa => empresas.id == id);
-        
-            res.render('perfil_usuario', {empresa});
-        });
-
-        empresas.put('editar', function(req, res, next) {
-            const id = req.params.id;
-            let empresa = empresas.find(empresa => empresa.id == id);
-            usuario = req.body.usuario;
-            nome = req.body.nome;
-            cnpj = req.body.cnpj;
-            email = req.body.email;
-            telefone = req.body.telefone;
-            profissao = req.body.profissao;
-            parceira = req.body.parceira;
-            residuo = req.body.residuo;
-            endereco = req.body.endereco;
-                   
-            res.redirect('perfil_usuario');
-        });
-    },
-
-    //deletar:
-
-    //criarUser: precisa desse? Pra mim, guardar e criar são as mesmas coisas
-
-    guardarUser: function (req, res, next) {
-        let json_empresa = fs.readFileSync(path.join(_dirname, '..', 'data', 'empresas.json'));
-
-        empresas.post({
-            email: req.body.email,
-            cnpj: req.body.cnpj,
-            telefone: req.body.cnpj,
-            endereco: req.body.endereco,
-            numero: req.body.endereco,
-            bairro: req.body.bairro,
-            complemento: req.body.bairro,
-            cidade: req.body.cidade,
-            estado: req.body.estado,
-            cep: req.body.cep,
-            funcionamento: res.body.funcionamento
-        });
-
-        let novo_json_empresas = JSON.stringify(empresas)
-        fs.writeFileSync(path.join(__dirname, '..', 'data', 'empresas.json'), novo_json_empresas);
-        res.send('Operação efetuada com sucesso!');
-    },
-
-    
-}    
+        res.redirect('/perfilusuario');
+    }
+} 
